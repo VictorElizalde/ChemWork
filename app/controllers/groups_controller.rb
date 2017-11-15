4074f1220132c324149c2a1bff05979b892a51af
Victor Elizalde <vic-bull@hotmail.com>
2017-11-14 23:10:22 -0600

    Group form

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,25 @@
 class GroupsController < ApplicationController
 	before_action :authenticate_user!
-	def index
 
+	def index
 	end
 
 	def new
-
+		@group = Group.new
 	end
 
 	def create
+		@group = Group.new(group_params)
 
+		if @group.save
+		    redirect_to @groups
+		  else
+		    render 'new'
+		  end
 	end
+
+	private
+  def group_params
+    params.require(:group).permit(:name, :schedule)
+  end
 end
