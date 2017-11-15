dbc5e83b8a6e3489e74beb3a84dc07ca724ad165
Victor Elizalde <vic-bull@hotmail.com>
2017-11-14 19:16:05 -0600

    Role and validation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+	before_action :authenticate_user!
+	def index
+
+	end
+
+	def new
+
+	end
+
+	def create
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	resources :groups
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
 	root 'groups#index'
-	
+
 end
