ef8a7d7b0ac2d55fe3517d952b7cf797501360da
Victor Elizalde <vic-bull@hotmail.com>
2017-11-15 15:07:34 -0600

    Reports form

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,25 @@
 class ReportsController < ApplicationController
+	before_action :authenticate_user!
+
+	def new
+		@group = Group.find(params[:group_id])
+		@assignment = @group.assignments.find(params[:assignment_id])
+		@report = Report.new
+	end
+
+	def create
+
+
+	end
+
+	def show
+	  send_data(@document.file_contents,
+	            type: @document.content_type,
+	            filename: @document.filename)
+	end
+
+	private
+  def report_params
+    params.require(:report).permit(:lab_phase, :file, :user_id, :assignment_id, :shared_with)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	resources :groups do
-		resources :assignments
+		resources :assignments do
+			resources :reports
+		end
 	end
+	
   devise_for :users, :controllers => { registrations: 'registrations' }
 
 	root 'groups#index'
